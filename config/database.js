97fa0dc63cf362e8850fee14c326ad2d7dc5c5b4
Mustafa <mustafa05deniz@gmail.com>
2017-08-06 15:05:29 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ module.exports = {
     'connection': {
         'host': '127.0.0.1', // localde iseniz bu , local değilse ip adresini yazınız .
         'user': 'root', // kullanıcı adı 
-        'password': '68126812', // şifreniz 
-        'database': 'kodizim'// database ismi .
+        'password': 'password', // şifreniz 
+        'database': 'dbname'// database ismi .
     },
-	'database': 'kodizim',
+	'database': 'dbname',
     
 };
 
